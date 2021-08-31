6c60d536aceb195f42544448993c492c4632a5a8
PurushYM <purushottam.yadav@yellowmessenger.com>
2021-08-31 09:56:27 +0530

    Project setup completed for push notifications

--- app/src/main/java/com/yellowmessenger/ymchatbotexample/MainActivity.kt
+++ app/src/main/java/com/yellowmessenger/ymchatbotexample/MainActivity.kt
@@ -3,20 +3,57 @@ package com.yellowmessenger.ymchatbotexample
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.messaging.FirebaseMessaging
 import com.yellowmessenger.ymchat.YMChat
 import com.yellowmessenger.ymchat.YMConfig
 import com.yellowmessenger.ymchat.models.YMBotEventResponse
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var botId = "x1587041004122"
+
+    private var TAG: String = "YMLog"
+    private var botId = "x1608615889375"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val theme = this.theme
+        showExtraNotificationData()
         initChatBot()
+    }
+
+    private fun getDataFromIntent(): MutableMap<String, Any?> {
+        val data = mutableMapOf<String, Any?>()
+        if (intent.extras != null) {
+            for (key in intent.extras!!.keySet()) {
+                val value = intent.extras!![key]
+                data[key] = value
+            }
+            this.intent.putExtras(Bundle.EMPTY)
+        }
+        return data
+    }
+
+    private fun showExtraNotificationData() {
+        if (intent.extras != null && intent.extras?.getString("botId") != null) {
+            val data = getDataFromIntent()
+            showNotificationAlert(intent.extras?.getString("botId").toString())
+        }
+    }
+
+    private fun showNotificationAlert(botId: String) {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this);
+        alertDialog.setTitle("Notification")
+        alertDialog.setMessage("Bot id : $botId")
+        alertDialog.setPositiveButton("Ok") { dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+        alertDialog.show()
+
     }
 
     private fun initChatBot() {
@@ -30,7 +67,7 @@ class MainActivity : AppCompatActivity() {
         payloadData["some-key"] = "some-value"
         payloadData["UserId"] = "qweqwe123214qwee23424"
         ymChat.config.payload = payloadData
-        ymChat.config.enableHistory = true
+//        ymChat.config.enableHistory = true
 
         // To Change the color of status bar, by default it will pick app theme
         ymChat.config.statusBarColor = R.color.teal_700
@@ -45,6 +82,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
         ymChat.onBotClose { Log.d("Example App", "Bot Was closed") }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+            Log.d(TAG, token.toString())
+            ymChat.config.deviceToken = token.toString()
+        })
 
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
---
+++ app/src/main/java/com/yellowmessenger/ymchatbotexample/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package com.yellowmessenger.ymchatbotexample
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    final var TAG:String = "YMLog"
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.i(TAG+" Remote message", remoteMessage.toString())
+        Log.i(TAG+" Remote message", remoteMessage.data.toString())
+        super.onMessageReceived(remoteMessage)
+    }
+}
